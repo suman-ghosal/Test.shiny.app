9af21de1b8371643d194d334f33986db3f3676f9
Suman Ghosal <suman.ghosal@nih.gov>
2024-12-24 15:39:08 -0500

    Delete tests directory

--- tests/testthat/test-server.R
+++
@@ -1,7 +0,0 @@
-testServer(
-  expr = {
-  # Check correct default values of the inputs can be set
-  session$setInputs(xcol="Sepal.Length", ycol="Sepal.Width", clusters=3)
-
-    expect_vector(clusters()$cluster)
-})
